451e0a751c87b15b6dfd68142b9fca5270bff280
Hanif Pratama W <Aruna_012@Arunas-MacBook-Pro-2.local>
2023-02-10 16:34:39 +0700

    update color

--- components/Billboard/index.js
+++ components/Billboard/index.js
@@ -3,16 +3,16 @@ import React from 'react'
 const Billboard = () => {
   return (
     <div style={{width: "100%", margin: 20 }}>
-            <div style={{display: "flex", borderColor: "grey", justifyContent: "center", alignItems: "center"}}>
-            <div style={{display: "flex",  borderStyle: "solid", justifyContent: "center", alignItems: "center", width: 970, height: 50, backgroundColor: "#DCDCDC"}}>
-                <h3>Top Frame 970x50</h3>
+            <div style={{display: "flex",  justifyContent: "center", alignItems: "center"}}>
+            <div style={{display: "flex", borderColor: "grey",  borderStyle: "solid", justifyContent: "center", alignItems: "center", width: 970, height: 50, backgroundColor: "#DCDCDC"}}>
+                <h3 style={{color: "grey"}}>Top Frame 970x50</h3>
             </div>
             </div>
 
             <div style={{marginTop: 24, display: "flex", justifyContent: "center", alignItems: "center"}}>
 
-            <div style={{display: 'flex', justifyContent: "center", alignItems: "center", width: 970, height: 250, backgroundColor: "#DCDCDC"}}>
-                <h3>Billboard 970x250</h3>
+            <div style={{display: 'flex',borderColor: "grey",  borderStyle: "solid", justifyContent: "center", alignItems: "center", width: 970, height: 250, backgroundColor: "#DCDCDC"}}>
+                <h3 style={{color: "grey"}}>Billboard 970x250</h3>
             </div>
             </div>
     </div>
